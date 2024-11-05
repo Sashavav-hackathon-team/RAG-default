bc7e35d99faa1200ded6e94b7f63080458cde5af
Sashavav <vav110011@gmail.com>
2024-11-06 00:11:32 +0300

    Add cosineSimilarity

--- Chunker/user-bge-m3-server.py
+++ Chunker/user-bge-m3-server.py
@@ -8,7 +8,27 @@ def get_embeddings(texts, model="BAAI/bge-m3"):
     texts = [text.replace("\n", " ") for text in texts]
     return client.embeddings.create(input=texts, model=model).data
 
+
+import numpy as np
+import scipy.spatial.distance as ds
+import torch.nn as nn
+import torch
+
 # Example usage
 texts = ["What is BGE M3?", "Definition of BM25"]
 embeddings = get_embeddings(texts)
-print(embeddings)
+vector_1 = np.array(embeddings[0].embedding)
+input1 = torch.tensor(embeddings[0].embedding)
+texts = ["How does BGE M3 work?", "Main purpose of BM25"]
+embeddings = get_embeddings(texts)
+#print(*embeddings[0].embedding)
+
+vector_2 = np.array(embeddings[0].embedding)
+
+dis = ds.cosine(vector_1, vector_2)
+print(dis)
+cos = nn.CosineSimilarity(dim=0, eps=1e-6)
+
+input2 = torch.tensor(embeddings[0].embedding)
+output = cos(input1, input2)
+print(output)
