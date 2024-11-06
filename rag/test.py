edcf99131185fecdbf7e27b9c36ace71a0552004
Vair <varvara.volodina@gmail.com>
2024-11-06 19:33:30 +0300

    remove file

--- rag/test.py
+++
@@ -1,5 +0,0 @@
-import utils.pdf_processing
-import pandas as pd
-
-array = pd.DataFrame(utils.pdf_processing.make_it_readable("/home/vairisw/python/RAG-default/rag/data/digital_design_and_computer_architecture_russian_translation_July16.pdf"))
-print(array.head())
